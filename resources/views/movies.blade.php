2f5d782811f195462d3226ad735cf425782ae9f2
Eugenio2104 <109421031+Eugenio2104@users.noreply.github.com>
2023-01-09 18:19:38 +0100

    stampa card

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,4 +1,27 @@
 @extends('layout.main')
 
 @section('content')
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-4">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h1 class="card-title">{{ $movie->title }}</h1>
+                            <p class="card-text">{{ $movie->original_title }}</p>
+                            <p class="card-text">{{ $movie->vote }}</p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
+
+
+{{-- "id" => 2
+"title" => "Via Col Vento"
+"original_title" => "Gone With The Wind"
+"nationality" => "american"
+"date" => "1939-12-15"
+"vote" => 8.6 --}}
